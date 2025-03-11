558011009444f42a2ad4814cd117cb336dbd76ca
xsw0803 <xsw0803@outlook.com>
2025-03-11 09:49:41 +0800

    first add

---
+++ Practical4/variables.py
@@ -0,0 +1,14 @@
+a = 15
+b = 75
+c = a + b
+d = 90
+e = 5
+f = d + e
+if c > f:
+    print (f"Car menthod takes{f} mins, is faster!")
+elif c == e:
+    print ("The same.")
+else:
+    print (f"Bus method takes{c} mins, is faster!")
+
+# Walk to bus stop and take the bus is quicker.
