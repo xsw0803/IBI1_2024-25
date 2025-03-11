978f8844fa3c8571970e78fd48fbcaa424430d09
xsw0803 <xsw0803@outlook.com>
2025-03-11 11:22:21 +0800

    change codes to simplify

--- Practical4/triangle.py
+++ Practical4/triangle.py
@@ -2,17 +2,13 @@ i = 0
 n = 0
 
 #to run the while looop
-while 1==1 :
+while i<= 10 :
     #for each loop, i represent the number of loops
     i += 1
-    
     #to print only ten numbers
-    if i <= 10:
-        #n is the output, each time to add the ordinal numbers of this number.
-        n += i
-        print (f"The sequence is {n}.")
-    else:
-        quit
+    #n is the output, each time to add the ordinal numbers of this number.
+    n += i
+    print (f"The sequence is {n}.")
 
 # Number i represents the first, second, third... figures.
 # Answer n represents the output of this calculation.
