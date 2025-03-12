f033e04b59084eda7598cd949aacdcbb198523b3
xsw0803 <xsw0803@outlook.com>
2025-03-12 14:00:19 +0800

    change n to Tn as sequence

--- Practical4/triangle.py
+++ Practical4/triangle.py
@@ -1,12 +1,12 @@
 i = 0 
-n = 0
+Tn = 0
 #to run the while loop and only give 10 results
 while i<= 10 :
     #for each loop, i represent the number of loops
     i += 1
     #n is the output, each time to add the ordinal numbers of this number.
-    n += i
-    print (f"The sequence is {n}.")
+    Tn += i
+    print (f"The sequence is {Tn}.")
 
 # Number i represents the first, second, third... figures.
 # Answer n represents the output of this calculation.
