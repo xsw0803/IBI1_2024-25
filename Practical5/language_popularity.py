942d4c6794ea4274f5632ac33a0b93bf52f64a3d
xsw0803 <xsw0803@outlook.com>
2025-03-18 10:50:24 +0800

    add changes and correct

--- Practical5/language_popularity.py
+++ Practical5/language_popularity.py
@@ -2,6 +2,10 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 
+'''
+create a dictionary about input language and popularity.
+Print the pupolarity of Python from the dictionary.
+'''
 Language_popularity = {
     'JavaScript': 62.3,
     'HTML': 52.9,
@@ -12,19 +16,23 @@ Language_popularity = {
 
 print(Language_popularity.get('Python'))
 
+#create a dataframe on language and popularity
 df = pd.DataFrame({"Language": Language_popularity.keys(), 
                    "Popularity": Language_popularity.values()})
 
+#put language into x and popularity into y
 x = list(Language_popularity.keys())
 y = list(Language_popularity.values())
 
-
+#draw the plot by x,y, color is skyblue and transquency is 1.0
+#yticks is from 0 to 80, step is 10
 p1 = plt.bar(x, y, color='skyblue', alpha=1.0)
 plt.xlabel('Language')
 plt.ylabel('Popularity')
 plt.title('The poularity of top5 input language')
 plt.yticks(range(0,81,10))
 
+#add text for each value. Show them in the bar plot.
 for bar in p1:
     height = bar.get_height()
     plt.text(
