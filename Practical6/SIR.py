ba4eb09bdc9c33559a45381525a328c92fdfd71f
xsw0803 <xsw0803@outlook.com>
2025-03-25 12:51:12 +0800

    change to guidance requirement

--- Practical6/SIR.py
+++ Practical6/SIR.py
@@ -1,41 +1,40 @@
 import numpy as np
-from scipy.integrate import odeint
 import matplotlib.pyplot as plt
 
-def sir_model(y, t, beta, gamma, N):
-    S, I, R = y
-    dSdt = -beta * S * I / N
-    dIdt = beta * S * I / N - gamma * I
-    dRdt = gamma * I
-    return [dSdt, dIdt, dRdt]
 
+N = 10000
+S = N - 1
+I = 1
+R = 0
 
-N = 10000         
-beta = 0.3        
-gamma = 0.05       
-initial_infected = 1 
-days = 200      
+beta = 0.3
+gamma = 0.05
 
+S_list = [S]
+I_list = [I]
+R_list = [R]
 
-S0 = N - initial_infected
-I0 = initial_infected
-R0 = 0
-y0 = [S0, I0, R0]
+time_steps = 1000
 
+for _ in range(time_steps):
+    infection_p = beta*I/N
+    new_I = np.random.choice([0, 1], S, p=[1-infection_p, infection_p]).sum()
+    new_R = np.random.choice([0, 1], I, p=[1-gamma, gamma]).sum()
+    S -= new_I
+    I += new_I - new_R
+    R += new_R
+    S_list.append(S)
+    I_list.append(I)
+    R_list.append(R)
 
-t = np.linspace(0, days, days)
-
-
-solution = odeint(sir_model, y0, t, args=(beta, gamma, N))
-S, I, R = solution.T  
-
-plt.figure()
-plt.plot(t, S, label='Susceptible')
-plt.plot(t, I, label='Infected')
-plt.plot(t, R, label='Recovered')
-plt.xlabel('Days')
-plt.ylabel('Population')
-plt.title(f'SIR Model: β={beta}, γ={gamma}, R0={beta/gamma:.2f}')
+plt.figure(dpi=150)
+plt.plot(S_list, label="Susceptible")
+plt.plot(I_list, label="Infected")
+plt.plot(R_list, label="Recovered")
+plt.xlabel("Time")
+plt.ylabel("Number of People")
+plt.title("SIR Model Simulation")
 plt.legend()
 plt.grid(True)
+plt.savefig("SIR_plot.png", format="png")
 plt.show()
