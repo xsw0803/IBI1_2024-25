0d2656b5209a361c3963aaa4ffb99affa87fe1f2
xsw0803 <xsw0803@outlook.com>
2025-03-31 11:07:33 +0800

    add True/False table

--- Practical4/variables.py
+++ Practical4/variables.py
@@ -15,10 +15,25 @@ else:
 # f is longer.
 # Walk to bus stop and take the bus is quicker.
 
-X = 1==1
-Y = 1==2
-W = 1==1 and 1==2 
-print (W)
+X1 = 1==1
+Y1 = 1==2
+W1 = X1 and Y1 
+print (W1)
+
+X2 = 1==1
+Y2 = 1==1
+W2 = X2 and Y2
+print(W2)
+
+X3 = 1==2
+Y3 = 1==2
+W3 = X3 and Y3
+print(W3)
+
+X4 = 1==2
+Y4 = 1==1
+W4 = X4 and Y4
+print(W4)
 
 # if x is true, y is false, w is false.
 # if x is true, y is true, w is true.
