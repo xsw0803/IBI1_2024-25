dc555c9ab1b16faf8f52950a0231f0124f98feb0
xsw0803 <xsw0803@outlook.com>
2025-03-31 11:11:50 +0800

    add pseudocode

--- Practical4/triangle.py
+++ Practical4/triangle.py
@@ -1,3 +1,13 @@
+'''
+I need a counter to serve as the order of nunmber in the sequence.
+And a output as the sequence.
+
+Then, the ordinal number will be bigger in length of 1 for each loop.
+The sequence number should plus the last ordinal number to form new sum.
+
+I use while loop, when conuter is smaller than 10, print the sequence numbers.
+'''
+
 i = 1 
 Tn = 0
 #to run the while loop and only give 10 results
@@ -6,7 +16,7 @@ while i<= 10 :
     Tn += i
     #for each loop, i represent the number of loops
     i += 1
-    print (f"The sequence is {Tn}.")
+    print (f"The number is {Tn}.")
 
 # Number i represents the first, second, third... figures.
 # Answer Tn represents the output of this calculation.
