c030b86d87e05cb62e74ab23c3bf97c09580fad6
xsw0803 <xsw0803@outlook.com>
2025-04-08 10:26:10 +0800

    correct calculation

--- Practical8/drug_dosage_calculator.py
+++ Practical8/drug_dosage_calculator.py
@@ -1,14 +1,16 @@
 weight = float(input("What's your weight(kg)?"))
-
-paracetamol = 120
+paracetamol = int(input("What's your paracetamol strength(mg/5ml)?\n120mg/5ml or 250mg/5ml"))
 
 def dosage(weight, paracetamol):
-    if 10 <= weight <=100:
-        paracetamol_pre = weight * 15
-        volume = (paracetamol_pre // paracetamol)
+    if paracetamol in [120, 250]:
+        if 10 <= weight <=100:
+            paracetamol_pre = weight * 15
+            volume = paracetamol_pre / paracetamol * 5
+        else:
+        
+            volume = 'Out of weight range.'
     else:
-        print('Out range of weight.')
-        volume = 'Out range of weight'
+        volume = 'Out of paracetamol strength range.'
     return volume
 
 print(dosage(weight, paracetamol))
