86bf7cef2f264eea7a984e05c1be5e66372d5e99
xsw0803 <xsw0803@outlook.com>
2025-05-20 16:28:58 +0800

    check and correct the output as whole sequence, not spliced sequence

--- Practical7/spliced_tata_genes.py
+++ Practical7/spliced_tata_genes.py
@@ -1,28 +1,28 @@
 import re
+import os
+import pandas as pd
 prompt = 'Which slice do you want?'
 prompt += '\nGTAG or GCAG or ATAC:'
 print(prompt)
 user_input = str(input())
-input = open('tata_genes.fa', 'r')
+
+os.chdir('/Users/xsw0803/Desktop/Programme/IBI_practicals/IBI1_2024-25/Practical7')
+input = open('Saccharomyces_cerevisiae.R64-1-1.cdna.all.fa', 'r')
 output = open(f'{user_input}_spliced_genes.fa', 'w')
+
+seq = ''
+gene = pd.DataFrame(columns=['Gene name', 'Gene sequence'])
+for line in input:
+    if re.search(r'>', line):
+        name = re.findall(r'gene:(.*?)\s', line)[0]
+        gene.loc[len(gene['Gene name'])] = [name, '']
+        seq = ''
+    else:
+        seq += line.rstrip()
+        gene.loc[len(gene['Gene name'])-1, 'Gene sequence'] = seq
 
 donor = user_input[:2]
 acceptor = user_input[-2:]
-print(donor, acceptor)
-
-sliced_list = []
-name_list = []
-gene_list = []
-for line in input:
-    if not re.search(r'>', line):
-        if re.findall(donor+r'.+'+acceptor, line):
-            sliced_seq = re.findall(donor+r'.+'+acceptor, line)
-            gene_list.append(line)
-            sliced_list.append(sliced_seq[0])
-        if not re.findall(donor+r'.+'+acceptor, line):
-            name_list.pop()
-    else:
-        name_list.append(line)
 
 def count_tata(seq):
     length = 7
@@ -33,15 +33,10 @@ def count_tata(seq):
             count += 1
     return count
 
-t = 0
-for i in range(0, len(sliced_list)):
-    judge = sliced_list[i]
-    t += 1
-    if t == 1:
-        if re.search(r'TATA[A|T]A[A|T]', judge):
-            output.write(f'{name_list[t-1][:-1]}  Number of TATA box instances is {count_tata(judge)}.\n')
-            output.write(gene_list[i][:-1])
-    else:
-        if re.search(r'TATA[A|T]A[A|T]', judge):
-            output.write(f'\n{name_list[t-1][:-1]}  Number of TATA box instances is {count_tata(judge)}.\n')
-            output.write(gene_list[i][:-1])
+for gene_seq in gene['Gene sequence']:
+    if re.search(r'TATA[A|T]A[A|T]', gene_seq):
+        if re.search(donor+r'.+'+acceptor, gene_seq):
+            gene_name = f'>{gene.loc[gene['Gene sequence'] == gene_seq, 'Gene name'].iloc[0]}'
+            output.write(f'{gene_name}\tNumber of TATA box is {count_tata(gene_seq)}.\n')
+            output.write(f'{gene_seq}\n')
+            continue
--- Practical7/tata_box.py
+++ Practical7/tata_box.py
@@ -1,44 +1,28 @@
 import re
+import os
+import pandas as pd
+os.chdir('/Users/xsw0803/Desktop/Programme/IBI_practicals/IBI1_2024-25/Practical7')
 input = open('Saccharomyces_cerevisiae.R64-1-1.cdna.all.fa', 'r')
-output = open('pre_data.fa', 'w')
-name_list = []
-seq = []
+output = open('tata_genes.fa', 'w')
+seq = ''
+gene = pd.DataFrame(columns=['Gene name', 'Gene sequence'])
 
-t = 0
 for line in input:
-    t +=1
-    if t == 1:
-        name1 = re.sub(r'>.+gene:', '', line)
-        name = re.sub(r'gene_.+]', '', name1)
-        output.write(f'>{name}')
-        name_list.append(f'{name}')
+    if re.search(r'>', line):
+        name = re.findall(r'gene:(.*?)\s', line)[0]
+        gene.loc[len(gene['Gene name'])] = [name, '']
+        seq = ''
     else:
-        if re.search(r'>', line):
-            if re.search(r'_mRNA', line):
-                name = re.sub(r'_mRNA.+]', '', line)
-                output.write(f'\n{name}')
-                name_list.append(f'{name}')
-            else:
-                name = re.sub(r'\s.+?]', '', line)
-                output.write(f'\n>{name}')
-                name_list.append(f'{name}')
-        else:
-            sequence = line[:-1]
-            output.write(f'{sequence}') 
+        seq += line.rstrip()
+        gene.loc[len(gene['Gene name'])-1, 'Gene sequence'] = seq
+
+for gene_seq in gene['Gene sequence']:
+    if re.search(r'TATA[A|T]A[A|T]', gene_seq):
+        tata_gene_name = gene.loc[gene['Gene sequence'] == gene_seq, 'Gene name'].iloc[0]
+        output.write(f'>{tata_gene_name}\n')
+        output.write(f'{gene_seq}\n')
+        
 input.close()
 output.close()
-
-input2 = open('pre_data.fa', 'r')
-output2 = open('tata_genes.fa', 'w')
-
-t = 0
-for line in input2:
-    print(line)
-    if not re.search(r'>', line):
-        t += 1
-        seq.append(line)
-        if re.search(r'TATA[A|T]A[A|T]', line):
-            output2.write(f'{name_list[t-1]}')
-            output2.write(f'{seq[t-1]}')
 
     
