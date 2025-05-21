ef99c87223c02ad63940ee20aa4cbd1715a54780
xsw0803 <xsw0803@outlook.com>
2025-05-21 12:01:46 +0800

    add to show the intron sequence

--- Practical7/largest_intron.py
+++ Practical7/largest_intron.py
@@ -2,5 +2,5 @@ import re
 seq = 'ATGCAAGTGGTGTGTCTGTTCTGAGAGGGCCTAA'
 intron = re.findall(r'GT.+AG', seq)
 print(seq)
-print(intron)
+print(f'The intron sequence is {intron[0]}.')
 print(f'The length of intron is {len(intron[0])} .')
